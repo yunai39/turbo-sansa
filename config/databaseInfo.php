682281abbd287ae463f8933cf71d0b023041d5f4
Yunai39 <yunai39@gmail.com>
2014-03-31 16:49:04 +0200

    Update databaseInfo.php

--- config/databaseInfo.php
+++ config/databaseInfo.php
@@ -5,4 +5,4 @@ $databaseInfo['default']['driver'] = 'mysql';
 $databaseInfo['default']['host'] = 'localhost'; 
 $databaseInfo['default']['db'] = 'testa'; 
 $databaseInfo['default']['username'] = 'root'; 
-$databaseInfo['default']['password'] = 'dragon'; 
+$databaseInfo['default']['password'] = ''; 
